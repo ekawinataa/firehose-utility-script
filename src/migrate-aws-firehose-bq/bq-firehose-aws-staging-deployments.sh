30f02cbb590ba77b4884072125d43bb1afe1a4fe
Eka Winata <eka.winata@gopay.co.id>
2024-07-22 18:19:05 +0700

    remove old script

--- src/migrate-aws-firehose-bq/bq-firehose-aws-staging-deployments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-kubectl config set-context main-goku-eks-staging-01
-kubectl config set-context --current --namespace=firehose
-deployments=($(kubectl get deployment | grep '^gopay-gl-.*-bigquery-.*' | awk '{print $1}'))
-rm -rf ./staging-resource
-mkdir ./staging-resource
-for deployment in "${deployments[@]}"; do
-   kubectl get deployment $deployment -o json > ./staging-resource/$deployment.json;
-done
